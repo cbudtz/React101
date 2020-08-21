cdaec76690f77a4dc3a52841e54ce1dcda3a406c
Christian Budtz <chrbudtz@gmail.com>
2020-08-21 10:26:48 +0200

    new ToDO List

---
+++ react1/todolist/src/App.js
@@ -0,0 +1,33 @@
+import React, {useState} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([{name:"Remember Milk",done:false},{name: "Remeber Hats",done:true}]);
+  const [input, setInput] = useState("")
+    function toggleTodo(key) {
+      let newState = [...todos];
+      newState[key].done=!newState[key].done
+      setTodos(newState)
+    }
+
+    function addTodo() {
+        let newTodoState = [...todos];
+        newTodoState.push({name: input,done:false})
+        setTodos(newTodoState);
+        setInput("");
+    }
+
+    return (
+      <div>
+        {todos.map((todo,key)=>
+            <div><button onClick={() => toggleTodo(key)}>Toggle Done</button>{todo.name} - {todo.done ? "Done" :"NotDone"} </div>
+        )}
+        <label>New Todo</label>
+        <input value={input} onChange={(e)=>setInput(e.target.value)}/>
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+  );
+}
+
+export default App;
